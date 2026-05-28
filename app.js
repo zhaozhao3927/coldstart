9ff302d2c78522998acd547399938073042ddd94
Yuqing Zhao <yu2511zh@lu.se>
2026-05-28 12:23:00 +0200

    fix: add single-quote escaping to escapeHtml

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ function escapeHtml(str) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 function init() {
